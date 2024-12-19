4a2a4fb10103bcd6a6ea075a9b52d2dc195e1bf7
gracemann365 <gracemann365@gmail.com>
2024-12-20 00:22:17 +0530

    someThing iDont Want You All to No

--- ChatbotServer.java
+++ ChatbotServer.java
@@ -10,9 +10,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatbotServer {
 
-    private static final String API_URL = "https://api.groq.com/openai/v1/chat/completions";
-    private static final String API_KEY = "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";
-    private static final String MODEL = "llama3-8b-8192";
+    private static final String API_URL = "Get Your Own URL bro ! ";
+    private static final String API_KEY = "You Seriously Thought I would give you my API Key ? ";
+    private static final String MODEL = "Find Your Own Damn Model Bro";
 
     private static final Map<String, StringBuilder> conversations = new ConcurrentHashMap<>();
 
